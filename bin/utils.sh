a542ad0f85520344837c1c4c33e1b5684b400068
Mitchell Hanberg <mitch@mitchellhanberg.com>
2020-11-19 12:23:54 -0500

    Fix shellcheck errors

--- bin/utils.sh
+++ bin/utils.sh
@@ -14,36 +14,34 @@ download_path() {
 }
 
 download_version() {
-  local version=$1
-  local download_path=$2
-  local download_url=$(download_url $version)
+  local install_type=$1
+  local version=$2
+  local download_path=$3
+  local download_url
 
-  rm $download_path
+  download_url=$(download_url "$install_type" "$version")
 
-  echo "curl -Lo $download_path -C - $download_url"
-  curl -Lo $download_path -C - $download_url || exit 1
+  rm "$download_path"
+
+  echo "==> curl -Lo $download_path -C - $download_url"
+  curl -Lo "$download_path" -C - "$download_url" || exit 1
 }
 
 download_url() {
+  local install_type=$1
+  local version_arg=$2
+  local version
+  if [[ "${version_arg}" =~ ^[0-9]+\.* ]] ; then
+    version="v$2"
+  else
+    version="$2"
+  fi
+
   if [ "$install_type" = "version" ]; then
-    local version_arg="$1"
-    if [[ "$version_arg" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-      local version="v$1"
-    else
-      local version="$1"
-    fi
     echo "https://github.com/neovim/neovim/releases/download/${version}/nvim-${platform}.tar.gz"
   else
-    local version
-    version=$1
-
-    if [[ "${version}" =~ ^[0-9]+\.* ]] ; then
-      # if version is a release number, prepend v
-      echo "https://github.com/neovim/neovim/archive/v${version}.tar.gz"
-    else
-      # otherwise it can be a branch name or commit sha
-      echo "https://github.com/neovim/neovim/archive/${version}.tar.gz"
-    fi
+    # otherwise it can be a branch name or commit sha
+    echo "https://github.com/neovim/neovim/archive/${version}.tar.gz"
   fi
 
   return
