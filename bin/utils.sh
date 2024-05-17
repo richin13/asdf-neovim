26998c8e0eb67d541963fd8ddf90902d4ae3724b
Kieran Coker <kieran.coker@rightmove.co.uk>
2024-05-17 13:33:11 +0100

    fix: add a check for version less that 0.10.x
    If the version is less than 0.10.x we will want to use the old platform
    of just `macos` rather than split based on arch

--- bin/utils.sh
+++ bin/utils.sh
@@ -36,14 +36,23 @@ download_url() {
 		platform=linux64
 		;;
 	"Darwin")
-		case "$(uname -p)" in
-		"arm")
-			platform=macos-arm64
-			;;
-		*)
-			platform=macos-x86_64
-			;;
-		esac
+		# Check version is less than v0.10.0 - if it is, use the old platform name
+		# using IFS to split the version number into an array
+		IFS='.' read -r -a version_array <<<"${version#v}"
+
+		# version_array[0] is the major version and version_array[1] is the minor version
+		if ((version_array[0] == 0 && version_array[1] < 10)); then
+			platform=macos
+		else
+			case "$(uname -p)" in
+			"arm")
+				platform=macos-arm64
+				;;
+			*)
+				platform=macos-x86_64
+				;;
+			esac
+		fi
 		;;
 	esac
 
